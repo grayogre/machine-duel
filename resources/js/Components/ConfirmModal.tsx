cc9ca68edcf03f011dbb14b6fa2506294e805ea7
grayogre <grayogre@gmail.com>
2023-08-25 10:41:10 +0900

    ComfirmModalのスタイル調整

--- resources/js/Components/ConfirmModal.tsx
+++ resources/js/Components/ConfirmModal.tsx
@@ -49,18 +49,18 @@ const ConfirmModal = memo((props: Props) => {
     <div className={'fixed flex flex-col items-center justify-center overflow-hidden bg-gray-500/50 transition-all z-40 ' + 
       (viewFlag ? ' top-0 left-0 h-screen w-screen ' : ' top-1/2 left-1/2 h-0 w-0 ')
     } > 
-      <div className="w-80 h-40 flex flex-col justify-between bg-white">
-      <div className="flex justify-start">
+      <div className="w-80 h-40 flex flex-col justify-between bg-white border border-gray-800 rounded-xl ">
+      <div className="flex justify-start p-2">
           <span className="block">{title}</span>
         </div>
         <div className="flex justify-center items-center">
           <span className="block p-4">{message}</span>
         </div>
-        <div className="flex justify-around">
-          <button className="inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white hover:bg-blue-500 focus:bg-blue-500 active:bg-blue-900" onClick={() => close(true)}>
+        <div className="flex justify-around py-2">
+          <button className="inline-flex items-center w-20 justify-center py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white hover:bg-blue-500 focus:bg-blue-500 active:bg-blue-900" onClick={() => close(true)}>
             はい
           </button>
-          <button className="inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white hover:bg-blue-500 focus:bg-blue-500 active:bg-blue-900"
+          <button className="inline-flex items-center w-20 justify-center py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white hover:bg-blue-500 focus:bg-blue-500 active:bg-blue-900"
             onClick={() => close(false)}
           >
             いいえ
